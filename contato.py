a13d31f46482a6088c4c7bd116ad1c10143c3eda
Letalock <leticia.dias24051@gmail.com>
2025-07-28 23:46:13 -0300

    atualizando contato

--- contato.py
+++ contato.py
@@ -77,4 +77,7 @@ def receber_contato():
         return jsonify({"erro": "Falha ao enviar o e-mail."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # ou 8080 se preferir
+    app.run(host='0.0.0.0', port=port)
+
